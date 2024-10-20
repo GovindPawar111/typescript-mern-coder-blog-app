5140173596f6ffd465f41a1177f11a69c6b8770e
Govind Pawar <pawargovind512@gmail.com>
2024-10-20 12:58:28 +0530

    changed the footer text

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
 const Footer: React.FC = () => {
-    const currentYear = new Date().getFullYear()
     return (
         <footer className="w-full bg-black text-white py-6 px-8 md:py-6 md:px-[200px] mt-auto">
             <p className=" text-center">
-                <span>Made with ❤️ by Govind</span>
-                <span> | All right reserved &#169;</span>
-                <span>{` CoderBlog ${currentYear}`}</span>
+                <span>Designed and developed with ❤️ by Govind.</span>
             </p>
         </footer>
     )
