a4adf863a1a5aa7cedaea8e81bb38fa9d2e6c4d4
Govind Pawar <pawargovind512@gmail.com>
2024-12-15 15:37:18 +0530

    changed the path of deploy command in package

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), svgr()],
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+    console.log(mode)
+    return {
+        plugins: [react(), svgr()],
+        base: !isDev ?  '/':'/typescript-mern-coder-blog-app/',
+    }
 })
